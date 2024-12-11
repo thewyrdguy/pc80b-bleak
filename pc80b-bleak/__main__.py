3ada6cd6ed1704c7fa87c3bd307db5211e5baa8f
Peter Wyrd <thewyrdguy@gmail.com>
2024-12-11 19:59:16 +0100

    Attempt to support gobject 3.50+ w/o gbulb

--- pc80b-bleak/__main__.py
+++ pc80b-bleak/__main__.py
@@ -1,18 +1,24 @@
 import asyncio
-import gbulb
-import gi
 
-gi.require_version("Gtk", "4.0")
-gi.require_version("Gst", "1.0")
-from gi.repository import Gst, Gtk
+try:
+    from gi.events import GLibEventLoopPolicy
+
+    asyncio.set_event_loop_policy(GlibEventLoopPolicy())
+    run_natively = True
+except ImportError:
+    import gbulb
+    import gi
+
+    gi.require_version("Gtk", "4.0")
+
+    gbulb.install(gtk=True)
+    run_natively = False
 
 from .gst import Pipe
 from .gui import App
 from .ble import scanner
 
 if __name__ == "__main__":
-    gbulb.install()
-
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     pipe = Pipe()
@@ -20,6 +26,9 @@ if __name__ == "__main__":
     app = App(pipe, application_id="wyrd.pc80b-bleak")
     loop.create_task(scanner(app))
     try:
-        loop.run_forever(application=app)
+        if run_natively:
+            app.run()
+        else:
+            loop.run_forever(application=app)
     except KeyboardInterrupt:
         app.quit()
