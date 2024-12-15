6f8ac4a9b066e9888ab9d3983675925742beeab3
Peter Wyrd <thewyrdguy@gmail.com>
2024-12-15 21:15:37 +0100

    WIP move data acquisition into a thread
    asyncio with combined mainloop between data acquision and gtk/gst almost
    works, but gtk/gst yields too infrequently (only a few times a second),
    clearly not enough to supply data smoothly, or even to receive it
    reliably over BLE.

--- pc80b-bleak/__main__.py
+++ pc80b-bleak/__main__.py
@@ -1,44 +1,22 @@
 """Command line entry point for pc80b-bleak streamer"""
 
-from asyncio import new_event_loop, set_event_loop
 from getopt import getopt
 from sys import argv
-
-try:
-    from gi.events import GLibEventLoopPolicy
-    from asyncio import (  # pylint: disable=ungrouped-imports
-        set_event_loop_policy,
-    )
-
-    set_event_loop_policy(GLibEventLoopPolicy())
-    RUN_NATIVELY = True
-except ImportError:
-    import gbulb
-    import gi
-
-    gi.require_version("Gtk", "4.0")
-
-    gbulb.install(gtk=True)
-    RUN_NATIVELY = False
+from threading import Thread
 
 # pylint: disable=relative-beyond-top-level  # Why is it complaining?...
 from .gst import Pipe
 from .gui import App
-from .ble import scanner, testsrc
+from .ble import Scanner
 
 if __name__ == "__main__":
     topts, args = getopt(argv[1:], "vt")
     opts = dict(topts)
-    sigsrc = testsrc if "-t" in opts else scanner
-    set_event_loop(loop := new_event_loop())
     (pipe := Pipe()).set_state(True)
-    loop.create_task(
-        sigsrc(app := App(pipe, application_id="wyrd.pc80b-bleak"))
-    )
+    dathread = Scanner((app := App(pipe)), **opts)
     try:
-        if RUN_NATIVELY:
-            app.run()
-        else:
-            loop.run_forever(application=app)
+        dathread.start()
+        app.run()
     except KeyboardInterrupt:
+        # TODO kill dathread
         app.quit()
