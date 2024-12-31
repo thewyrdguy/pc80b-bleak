f375c18b1d739838bf5c8d2a4c10a267ad3d2f1b
Peter Wyrd <thewyrdguy@gmail.com>
2024-12-31 21:50:19 +0100

    rearrange high level structure of the app

--- pc80b-bleak/gui.py
+++ pc80b-bleak/gui.py
@@ -6,6 +6,9 @@ gi.require_version("Gtk", "4.0")
 gi.require_version("Adw", "1")
 from gi.repository import Adw, Gtk
 
+from .gst import Pipe
+from .ble import Scanner
+
 CRT_W = 720
 CRT_H = 480
 
@@ -13,11 +16,14 @@ Gtk.init()
 
 
 class AppWindow(Gtk.ApplicationWindow):
-    def __init__(self, pipe, *args: Any, **kwargs: Any) -> None:
-        super().__init__(*args, **kwargs)
-        self.pipe = pipe
-        self.pipe.register_on_level_callback(self.on_level)
+    def __init__(self, app, *args: Any, **kwargs: Any) -> None:
+        self.args = args
+        self.kwargs = kwargs
+        super().__init__(application=app)
         self.level_data = {}
+        self.connect("close-request", self.on_close)
+        self.pipe = Pipe()
+        self.pipe.register_on_level_callback(self.on_level)
 
         # self.set_default_size(1080, 720)
         self.set_title("pc80b-bleak")
@@ -44,7 +50,7 @@ class AppWindow(Gtk.ApplicationWindow):
         hbox.append(lbox)
 
         picture = Gtk.Picture.new()
-        picture.set_paintable(pipe.paintable)
+        picture.set_paintable(self.pipe.paintable)
         frame = Gtk.Frame()
         frame.set_child(picture)
         crt = Gtk.Box()
@@ -54,6 +60,10 @@ class AppWindow(Gtk.ApplicationWindow):
 
         rbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
         hbox.append(rbox)
+
+        self.pipe.set_state(True)
+        self.datathread = Scanner(self, **kwargs)
+        self.datathread.start()
 
     def draw_mon(self, monda, c, w, h, _):
         maxh = h - 30
@@ -69,6 +79,10 @@ class AppWindow(Gtk.ApplicationWindow):
         c.rectangle(15, 15 + maxh - lvl, w - 30, lvl)
         c.fill()
 
+    def on_close(self, _) -> None:
+        self.datathread.stop()
+        self.pipe.set_state(None)
+
     def on_level(self, **kwargs: Any) -> None:
         self.level_data = kwargs
         self.monda.queue_draw()
@@ -76,50 +90,24 @@ class AppWindow(Gtk.ApplicationWindow):
     def report_ble(self, connected: bool, state: str) -> None:
         # show red/green indicator
         self.label.set_text(state)
-
-
-class App(Adw.Application):
-    def __init__(self, pipe, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.pipe = pipe
-        self.win = None
-        self.pending_ble_report = None
-
-        self.connect("activate", self.on_activate)
-        self.connect("shutdown", self.on_close)
-
-    def on_activate(self, app: Adw.Application) -> None:
-        assert app is self
-        self.win = AppWindow(self.pipe, application=app)
-        self.win.present()
-        if self.pending_ble_report is not None:
-            self.win.report_ble(*self.pending_ble_report)
-            self.pending_ble_report = None
-
-    def on_close(self, _):
-        self.win.close()
-        self.pipe.set_state(None)
+        self.pipe.report_ble(connected, state)
 
     def report_ecg(self, ev) -> None:
         self.pipe.report_ecg(ev)
 
-    def report_ble(self, connected: bool, state: str) -> None:
-        self.pipe.report_ble(connected, state)
-        if self.win is not None:
-            self.win.report_ble(connected, state)
-        else:
-            self.pending_ble_report = connected, state
 
+class App(Adw.Application):
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+        super().__init__()
+        self.connect("activate", self.on_activate)
+        self.connect("shutdown", self.on_shutdown)
 
-if __name__ == "__main__":
-    import sys
-    from .gst import Pipe
+    def on_activate(self, app: Adw.Application) -> None:
+        assert app is self
+        self.win = AppWindow(self, *self.args, **self.kwargs)
+        self.win.present()
 
-    pipe = Pipe()
-    pipe.set_state(True)
-    app = App(pipe, application_id="wyrd.pc80b-bleak")
-    try:
-        res = app.run(sys.argv)
-        print("exit", res)
-    except KeyboardInterrupt:
-        app.quit()
+    def on_shutdown(self, _):
+        self.win.close()
