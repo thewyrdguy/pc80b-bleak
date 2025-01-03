86348c4d9fd4004a122a9aef887efb2111ae92ca
Peter Wyrd <thewyrdguy@gmail.com>
2025-01-03 23:23:30 +0100

    gst: clean up pipeline construction

--- pc80b-bleak/gst.py
+++ pc80b-bleak/gst.py
@@ -14,7 +14,7 @@ CRT_H = 480
 CAPS = (
     f"video/x-raw,format=RGBA,bpp=32,depth=32,width={CRT_W},height={CRT_H}"
     ",red_mask=-16777216,green_mask=16711680,blue_mask=65280"
-    ",alpha_mask=255,endianness=4321,framerate=1/30"
+    ",alpha_mask=255,endianness=4321,framerate=30/1"
 )
 
 Gst.init()
@@ -33,19 +33,13 @@ class Pipe:
         self.paintable = gtksink.get_property("paintable")
         if not self.paintable.props.gl_context:
             raise RuntimeError("Refusing to run without OpenGL")
-        sink = Gst.ElementFactory.make("glsinkbin", None)
-        sink.set_property("sink", gtksink)
+        self.pipeline.add(gsnk := Gst.ElementFactory.make("glsinkbin", None))
+        gsnk.set_property("sink", gtksink)
+        gsnk.set_property("sync", True)
 
-        src = Gst.ElementFactory.make("appsrc", None)
-        src.set_property("format", Gst.Format.TIME)
-
-        self.pipeline.add(src)
-        self.pipeline.add(sink)
-
-        src.link_filtered(sink, Gst.Caps.from_string(CAPS))
-        sink.set_property("sync", True)
-
-        self.drw = Drw(src, CRT_W, CRT_H)
+        self.pipeline.add(appsrc := Gst.ElementFactory.make("appsrc", None))
+        appsrc.set_property("format", Gst.Format.TIME)
+        self.drw = Drw(appsrc, CRT_W, CRT_H)
 
         self.pipeline.add(
             asrc := Gst.ElementFactory.make("autoaudiosrc", None)
@@ -54,13 +48,12 @@ class Pipe:
             acnv := Gst.ElementFactory.make("audioconvert", None)
         )
         self.pipeline.add(alvl := Gst.ElementFactory.make("level", None))
-        self.pipeline.add(asnk := Gst.ElementFactory.make("fakesink", None))
+        self.pipeline.add(fakesnk := Gst.ElementFactory.make("fakesink", None))
+        fakesnk.set_property("sync", True)
         asrc.link(acnv)
         acnv.link_filtered(
             alvl, Gst.Caps.from_string("audio/x-raw,channels=2")
         )
-        alvl.link(asnk)
-        asnk.set_property("sync", True)
         bus.add_signal_watch()
         bus.connect("message::element", self.on_level)
 
@@ -71,16 +64,48 @@ class Pipe:
             self.pipeline.add(
                 rtmp := Gst.ElementFactory.make("rtmpsink", None)
             )
-            rtmp.set_property("location", "rtmp://localhost/stream live=1")
+            rtmp.set_property(
+                "location", "rtmp://localhost:9999/stream live=1"
+            )
             self.pipeline.add(
                 flvmux := Gst.ElementFactory.make("flvmux", None)
             )
+            flvmux.set_property("streamable", True)
             flvmux.link(rtmp)
-            self.pipeline.add(
-                flvenc := Gst.ElementFactory.make("avenc_flv", None)
-            )
-            flvenc.link(flvmux)
-            # src.link_filtered(flvenc, Gst.Caps.from_string(CAPS))
+
+            #self.pipeline.add(
+            #    voaacenc := Gst.ElementFactory.make("voaacenc", None)
+            #)
+            #voaacenc.set_property("bitrate", 128000)
+            #voaacenc.link(flvmux)
+            #alvl.link(voaacenc)
+
+            #self.pipeline.add(
+            #    x264enc := Gst.ElementFactory.make("x264enc", None)
+            #)
+            #x264enc.set_property("cabac", 1)
+            #x264enc.set_property("bframes", 2)
+            #x264enc.set_property("ref", 1)
+            #x264enc.link(flvmux)
+            ##self.pipeline.add(
+            ##    vqueue := Gst.ElementFactory.make("queue", None)
+            ##)
+            ##vqueue.link(x264enc)
+            #self.pipeline.add(
+            #    vconv := Gst.ElementFactory.make("videoconvert", None)
+            #)
+            #vconv.link(vqueue)
+
+            #appsrc.link_filtered(vconv, Gst.Caps.from_string(CAPS))
+            #self.pipeline.add(
+            #    tvsrc := Gst.ElementFactory.make("videotestsrc", None)
+            #)
+            #tvsrc.set_property("is-live", 1)
+            #tvsrc.link_filtered(vconv, Gst.Caps.from_string(CAPS))
+            #tvsrc.link(vconv)
+
+        alvl.link(fakesnk)
+        appsrc.link_filtered(gsnk, Gst.Caps.from_string(CAPS))
 
     def on_eos(self, bus, msg):
         print("End of stream")
